eb615d06db0fe37ef6ba1cefd1c6f97d437dca4f
imHyejinPark <qkrgpwls1010@gmail.com>
2020-08-19 17:06:42 +0900

    hp관리_수정
    setActive -> 이미지변경

--- Assets/Scripts/Haenyeo.cs
+++ Assets/Scripts/Haenyeo.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Globalization;
+using System.Diagnostics;
+using System.Runtime.Versioning;
 
 public class Haenyeo : MonoBehaviour
 {
     public static int money, debt, diving_time, moving_speed, farm_number, day, level, hp;
 
     public static int[] sea_item_number = new int[9]; //보유하고있는 자원 개수
-    public Image hp100, hp80, hp60, hp40, hp20, hp0;
     public Text hp_text;
+    public Image hp_bg;
+    public Sprite hp100, hp80, hp60, hp40, hp20, hp0;
 
     public enum sea_item_index
     {
@@ -31,57 +34,48 @@ public class Haenyeo : MonoBehaviour
     public void Start()
     {
         hp_text.GetComponent<Text>().text = hp.ToString();
+        Sprite hp100 = Resources.Load<Sprite>("heart100");
+        Sprite hp80 = Resources.Load<Sprite>("heart80");
+        Sprite hp60 = Resources.Load<Sprite>("heart60");
+        Sprite hp40 = Resources.Load<Sprite>("heart40");
+        Sprite hp20 = Resources.Load<Sprite>("heart20");
+        Sprite hp0 = Resources.Load<Sprite>("heart0");
     }
     void Update()
     {
+        
+       
         hp_text.GetComponent<Text>().text = hp.ToString();
-
         if (hp >= 90)
         {
-            hp_off();
-            hp100.gameObject.SetActive(true);
+            hp_bg.GetComponent<Image>().sprite = hp100;
         }
         else if (hp < 90 && hp >= 70)
         {
-            hp_off();
-            hp80.gameObject.SetActive(true);
+            hp_bg.GetComponent<Image>().sprite = hp80;
         }
         else if (hp < 70 && hp >= 50)
         {
-            hp_off();
-            hp60.gameObject.SetActive(true);
+            hp_bg.GetComponent<Image>().sprite = hp60;
         }
         else if (hp < 50 && hp >= 30)
         {
-            hp_off();
-            hp40.gameObject.SetActive(true);
+            hp_bg.GetComponent<Image>().sprite = hp40;
         }
         else if (hp < 30 && hp >= 10)
         {
-            hp_off();
-            hp20.gameObject.SetActive(true);
+            hp_bg.GetComponent<Image>().sprite = hp20;
         }
         else if (hp < 10)
         {
-            hp_off();
-            hp0.gameObject.SetActive(true);
+            hp_bg.GetComponent<SpriteRenderer>().sprite = hp0;
         }
-
-
     }
 
-    void hp_off()
+    void SetHP()
     {
-        hp100.gameObject.SetActive(false);
-        hp80.gameObject.SetActive(false);
-        hp60.gameObject.SetActive(false);
-        hp40.gameObject.SetActive(false);
-        hp20.gameObject.SetActive(false);
-        hp0.gameObject.SetActive(false);
+        
+        
     }
-
-
-
-
 
 }
