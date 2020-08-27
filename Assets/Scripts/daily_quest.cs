554ef3d38cab810fad407896419ca3ed8e1f1bf0
hyomin <gyals6349@gmail.com>
2020-08-28 00:26:23 +0900

    add

--- Assets/Scripts/daily_quest.cs
+++ Assets/Scripts/daily_quest.cs
@@ -2,26 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/*
-public class daily_quest_form
-{
-    public string summary;
-    public string type;
-    public int person;
-    public string text;
-    public string todo;
-    public string reward;
-
-    public daily_quest_form(string summary, string type,int person,string text,string todo,string reward) {
-        this.summary = summary;
-        this.type = type;
-        this.person = person;
-        this.text = text;
-        this.todo = todo;
-        this.reward = reward;
-    }
-}
-*/
 public class daily_quest : MonoBehaviour
 {
     public static Daily_quest_form daily_quest1 
