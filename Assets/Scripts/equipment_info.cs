24ae048602d09639d393644ca6f609bf29dbf4e3
haayun <kng06325@gmail.com>
2020-09-09 16:11:42 +0900

    sound + equip 수정

--- Assets/Scripts/equipment_info.cs
+++ Assets/Scripts/equipment_info.cs
@@ -8,10 +8,10 @@ public class equipment_info : MonoBehaviour//, IPointerDownHandler, IPointerUpHa
 {
     public Image equipment_info_obj; // 장비 레이아웃 
     public int next_upgrade_price;    // 다음 장비로 업그레이드에 필요한 가격. -1로 설정할 경우 다음 업그레이드가 없음.
-    public int speed_up; // 오리발만 해당, 나머지는 -1로 설정.
-    public int time_up; // 해녀복, 물안경 해당, 오리발은 -1로 설정.
+    //public int speed_up; // 오리발만 해당, 나머지는 -1로 설정.
+    //public int time_up; // 해녀복, 물안경 해당, 오리발은 -1로 설정.
     public Text equip_name;
-    public bool isInfoClick = false;
+    //public bool isInfoClick = false;
 
     //public void OnPointerDown(PointerEventData eventData)
     //{
--- Assets/Scripts/item_sell.cs
+++ Assets/Scripts/item_sell.cs
@@ -17,7 +17,6 @@ public class item_sell : MonoBehaviour
 
     public Image plus_money;
     public int temp_index, temp_num, temp_money;
-    //int if_no_wild = 0;
 
     private void OnEnable()
     {
@@ -25,10 +24,8 @@ public class item_sell : MonoBehaviour
     }
     void Awake()
     {
-        //bgm.volume = PlayerPrefs.GetFloat("Bgm_volume", 1);
         sell_click.volume = PlayerPrefs.GetFloat("Effect_volume", 1);
         updown_click.volume = PlayerPrefs.GetFloat("Effect_volume", 1);
-        //if_no_wild = 0;
         data_load();
         item_noshow();
         item_UI();        
@@ -106,7 +103,6 @@ public class item_sell : MonoBehaviour
         {
             if(Haenyeo.sea_item_number[i] > 0)
             {
-                //if_no_wild++;
                 no_wild.gameObject.SetActive(false);
                 sea_items[i].gameObject.SetActive(true);
 
@@ -122,10 +118,6 @@ public class item_sell : MonoBehaviour
                 down(i);
             }
         }
-        //if (if_no_wild == 0)
-        //{
-        //    no_wild.gameObject.SetActive(true);
-        //}
     }
 
     //모든 자원창을 끔
