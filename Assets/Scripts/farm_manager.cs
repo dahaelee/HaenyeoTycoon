79a25b64b3ed15745e75c5cae94aae2377f5bed5
SooMin Lee <vilut1002@gmail.com>
2020-12-30 13:14:45 +0900

    debug farm

--- Assets/Scripts/farm_manager.cs
+++ Assets/Scripts/farm_manager.cs
@@ -520,7 +520,11 @@ public class farm_manager : MonoBehaviour
 
             sending_int = Haenyeo.money;
             sending_str = sending_int.ToString();
-
+            if (sending_int > Haenyeo.debt)
+            {
+                sending_int = Haenyeo.debt;
+                sending_str = sending_int.ToString();
+            }
         }
         sending_amount_repay.GetComponent<Text>().text = string.Format("{0:#,###0}", sending_int);
     }
@@ -584,12 +588,12 @@ public class farm_manager : MonoBehaviour
             is_sea_locked = false;      //바다 아이콘 활성화
             PlayerPrefs.SetInt("is_repay_locked", 1);
 
-            if (Haenyeo.day > Haenyeo.limit_day && Haenyeo.debt > 0)
+            if (Haenyeo.day >= Haenyeo.limit_day && Haenyeo.debt > 0)
             {
                 StartCoroutine("bad_ending");
             }
 
-            if (Haenyeo.day > Haenyeo.limit_day && Haenyeo.debt < 1)
+            if (Haenyeo.day >= Haenyeo.limit_day && Haenyeo.debt < 1)
             {
                 StartCoroutine("happy_ending");
 
@@ -638,12 +642,12 @@ public class farm_manager : MonoBehaviour
         is_repay_locked = true;
         is_sea_locked = false;      //바다 아이콘 활성화
         
-        if (Haenyeo.day > Haenyeo.limit_day && Haenyeo.debt > 0)
+        if (Haenyeo.day >= Haenyeo.limit_day && Haenyeo.debt > 0)
         {
             StartCoroutine("bad_ending");
         }
 
-        if (Haenyeo.day > Haenyeo.limit_day && Haenyeo.debt <= 0)
+        if (Haenyeo.day >= Haenyeo.limit_day && Haenyeo.debt <= 0)
         {
             StartCoroutine("happy_ending");
 
